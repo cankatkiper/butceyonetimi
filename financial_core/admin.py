6978997fec3e4436ec2a751f2b170e5264e76a62
cankatkiper <cankatkiper24@gmail.com>
2025-05-12 12:25:51 +0300

    Delete financial_core directory

--- financial_core/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-# Soyut modeller doğrudan admin'e kaydedilmediği için boş bırakıyoruz
-
-# Register your models here.
--- financial_core/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinancialCoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "financial_core"
-    verbose_name = "Financial Core"
